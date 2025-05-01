60ba5cfc8e521213c2f91a6d507262f75c20d859
AlexTran0899 <alextran0899@gmail.com>
2025-05-01 11:34:22 -0500

    add health check

--- main.py
+++ main.py
@@ -102,6 +102,9 @@ def retrieve():
     results["data"] = final_results
     return jsonify(results)
 
+@app.route('/health', methods=['GET'])
+def health_check():
+    return 'OK', 200
 
 if __name__ == '__main__':
     app.run(debug=True)
